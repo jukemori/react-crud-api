8fac1a5d6cffe821a4b1cc5b401db96f8c02da0a
Jun Ukemori <jukemori@gmail.com>
2023-08-21 14:28:00 +0900

    set form component

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -6,11 +6,11 @@ const Form = (props) => {
       <form>
         <div>
           <label htmlFor="name">Name：</label>
-          <input type="text" name="name" id="name" onChange={(e) => handleChange(e)} value={value.name}/>
+          <input type="text" name="name" id="name" onChange={(e) => handleChange(e)} value={value.name  || ''}/>
         </div>
         <div>
           <label htmlFor="country">Country</label>
-          <input type="text" name="country" id="country" onChange={(e) => handleChange(e)} value={value.country}/>
+          <input type="text" name="country" id="country" onChange={(e) => handleChange(e)} value={value.country  || ''}/>
         </div>
         <input type="submit" value={buttonType} onClick={(e) => handleSubmit(e)}/>
       </form>
